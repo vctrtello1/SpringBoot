94b598b7caea48e15be01586176a49031f771f1f
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-05-22 19:13:26 -0600

    SuperContraGame Qualifier

--- src/main/java/com/victorTello/LearnSpringBoot/game/MarioGame.java
+++ src/main/java/com/victorTello/LearnSpringBoot/game/MarioGame.java
@@ -1,5 +1,10 @@
 package com.victorTello.LearnSpringBoot.game;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class MarioGame implements GamingConsole{
     public void up() {
         System.out.println("Jump");
