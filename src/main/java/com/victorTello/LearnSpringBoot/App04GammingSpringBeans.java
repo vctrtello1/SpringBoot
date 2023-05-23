77cde1aa705abc8e479664d6b9f9a4fda6be17b0
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-05-22 18:21:16 -0600

    Gamming configuration

--- src/main/java/com/victorTello/LearnSpringBoot/App04GammingSpringBeans.java
+++ src/main/java/com/victorTello/LearnSpringBoot/App04GammingSpringBeans.java
@@ -1,11 +1,30 @@
 package com.victorTello.LearnSpringBoot;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.victorTello.LearnSpringBoot.game.GameRunner;
 import com.victorTello.LearnSpringBoot.game.GamingConsole;
+import com.victorTello.LearnSpringBoot.game.PacmanGame;
 
-public class App03GamingSpringBeans {
+@Configuration
+class GamingConfiguration {
+
+    @Bean
+    public GamingConsole game() {
+        return new PacmanGame();
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
+
+}
+
+public class App04GammingSpringBeans {
+
     public static void main(String[] args) {
 
         try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
@@ -14,4 +33,5 @@ public class App03GamingSpringBeans {
         }
 
     }
+
 }
--- src/main/java/com/victorTello/LearnSpringBoot/GamingConfiguration.java
+++ src/main/java/com/victorTello/LearnSpringBoot/GamingConfiguration.java
@@ -3,6 +3,7 @@ package com.victorTello.LearnSpringBoot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.victorTello.LearnSpringBoot.game.GameRunner;
 import com.victorTello.LearnSpringBoot.game.GamingConsole;
 import com.victorTello.LearnSpringBoot.game.PacmanGame;
 
@@ -12,6 +13,11 @@ public class GamingConfiguration {
     @Bean
     public GamingConsole game() {
         return new PacmanGame();
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
     }
 
 }
