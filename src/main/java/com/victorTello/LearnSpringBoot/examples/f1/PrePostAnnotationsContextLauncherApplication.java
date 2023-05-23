de8594cc8f2e64a4c66fda19448f6e1fb528ae95
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-05-23 11:22:16 -0600

    PrePost Annotations

---
+++ src/main/java/com/victorTello/LearnSpringBoot/examples/f1/PrePostAnnotationsContextLauncherApplication.java
@@ -0,0 +1,49 @@
+package com.victorTello.LearnSpringBoot.examples.f1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+class SomeClass {
+
+    private SomeDependency someDependency;
+
+    public SomeClass(SomeDependency someDependency){
+        this.someDependency = someDependency;
+        System.out.println("All dependencies are ready!");
+    }
+
+    public void initialize(){
+        someDependency.getReady();
+    }
+
+}
+
+@Component
+class SomeDependency{
+
+    public void getReady() {
+        System.out.println("Some logic using SomeDependency");
+    }
+
+}
+
+@Configuration
+@ComponentScan
+public class PrePostAnnotationsContextLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context = new AnnotationConfigApplicationContext(PrePostAnnotationsContextLauncherApplication.class)) {
+
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        }
+
+    }
+
+}
