0079822ee39373266da9e509602482a9951e9452
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-05-25 14:39:48 -0600

    XMLConfigurationContext

--- src/main/java/com/victorTello/LearnSpringBoot/examples/h1/XMLConfigurationContextLauncherApplication.java
+++ src/main/java/com/victorTello/LearnSpringBoot/examples/h1/XMLConfigurationContextLauncherApplication.java
@@ -2,20 +2,22 @@ package com.victorTello.LearnSpringBoot.examples.h1;
 
 import java.util.Arrays;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
+import com.victorTello.LearnSpringBoot.game.GameRunner;
+
 public class XMLConfigurationContextLauncherApplication {
 
     public static void main(String[] args) {
 
-        try (var context = new AnnotationConfigApplicationContext(XMLConfigurationContextLauncherApplication.class)) {
+        try (var context = new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
 
             Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
+            // System.out
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+            context.getBean(GameRunner.class).run();
         }
 
     }
